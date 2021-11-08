156590895d9a30db79d3f493872ca1d97accceab
DESKTOP-PPFCO05\Z77 <example@example.com>
2021-11-08 10:51:20 -0500

    Add project files.

---
+++ App19/App19/Contact.cs
@@ -0,0 +1,44 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App19
+{
+    class Contact
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID
+        {
+            get;
+            set;
+        }
+        public string Firstname
+        {
+            get;
+            set;
+        }
+        public string Lastname
+        {
+            get;
+            set;
+        }
+        public string Email
+        {
+            get;
+            set;
+        }
+        public string Address
+        {
+            get;
+            set;
+        }
+
+        [MaxLength(10)]
+        public string PhoneNumber
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ App19/App19/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using System.IO;
+using Xamarin.Essentials;
+using SQLite;
+
+namespace App19
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        string _fileName = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "notes.txt");
+
+        public MainPage()
+        {
+            InitializeComponent();
+            var FilePath = Path.Combine(FileSystem.AppDataDirectory, "mydb.db");
+
+            using (SQLiteConnection conn = new SQLiteConnection(FilePath))
+            {
+                conn.CreateTable<Contact>();
+                //int rowsAdded = conn.Insert(contact); 
+                var contacts = conn.Table<Contact>().ToList();
+                contactsListView.ItemsSource = contacts;
+
+
+            }
+        }
+
+        
+
+        
+    }
+}
